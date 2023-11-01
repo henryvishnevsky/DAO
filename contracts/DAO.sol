68d4560f363947a69a08a93329f2825ea5e81596
henryvishnevsky <github.hn9cy@passmail.com>
2023-10-31 19:01:27 -0600

    Add voting function and tests

--- contracts/DAO.sol
+++ contracts/DAO.sol
@@ -28,6 +28,11 @@ contract DAO {
         address creator
     );
 
+    event Vote(
+        uint256 id,
+        address investor
+    );
+
     constructor(Token _token, uint256 _quorum) {
         owner = msg.sender;
         token = _token;
@@ -39,7 +44,7 @@ contract DAO {
 
     modifier onlyInvestor() {
         require(
-            Token(token).balanceOf(msg.sender) > 0,
+            token.balanceOf(msg.sender) > 0,
             "The proposer must be token holder"
         );
         _;
@@ -66,6 +71,27 @@ contract DAO {
         );
 
         emit Propose(proposalCount, _amount, _recipient, msg.sender);
+
+    }
+
+    mapping(address => mapping(uint256 => bool)) votes;
+
+    function vote(uint256 _id) external onlyInvestor {
+        //fetch proposal from mapping by id
+        Proposal storage proposal = proposals[_id];
+
+        //Check that the investor hasn't voted
+        require(!votes[msg.sender][_id], "Already voted");
+
+        //update votes
+        proposal.votes += token.balanceOf(msg.sender);
+
+        //Track that user has voted
+        votes[msg.sender][_id] = true;
+
+        //Emit an event
+        emit Vote(_id, msg.sender);
+
 
     }
 
